fac83b63748f19003aff33c9b4e538b7a572e589
Danchik <erznann@gmail.com>
2026-05-30 13:01:19 +0500

    fix: configure github pages deploy

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_PAGES === 'true' ? '/cofeeyaa/' : '/',
+  base: process.env.GITHUB_PAGES === 'true' ? process.env.GITHUB_PAGES_BASE ?? '/cofeeyaa/' : '/',
   plugins: [react()],
   server: {
     proxy: {
